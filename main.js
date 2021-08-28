6d61df388d2d11374513cdd88193791cc5e0f54c
abstractsins <18626012+abstractsins@users.noreply.github.com>
2021-08-28 03:03:22 -0400

    content complete
    all content complete...
    breakpoints still needed..

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 /*RESEARCH PREVIEW function*/
 //takes a section from research.html and places it in index.html
-jQuery(function($){
-	$('#result').load('research.html #research-preview');
+jQuery(function async($){
+	$('#current-research-preview-1').load('research.html #research-preview-1');
+});
+
+jQuery(function async($){
+	$('#current-research-preview-title-1').load('research.html #research-title-1');
 });
 
 /*GLOBAL VARIABLES*/
@@ -12,50 +16,51 @@ const research = document.getElementById('nav-link-research');
 const data = document.getElementById('nav-link-data');
 const teaching = document.getElementById('nav-link-teaching');
 const navLinks = [about, publications, research, data, teaching];
+const leadText = document.querySelector('lead-text');
 
 /*BOLDING functions for nav links*/
 //the function goes through the array of nav links looking for bolded ones, then unbolds them. Then it bolds the active nav item.
 about.addEventListener('click', function(){
     navLinks.forEach(element => {
-        if (element.style.fontWeight = 'bold') {
+        if (element.style.fontWeight = '800') {
             element.style.fontWeight = '500';
         }
     });
-    about.style.fontWeight = 'bold';
+    about.style.fontWeight = '800';
 });
 
 publications.addEventListener('click', function(){
     navLinks.forEach(element => {
-          if (element.style.fontWeight = 'bold') {
+          if (element.style.fontWeight = '800') {
             element.style.fontWeight = '500';
         }
     });
-    publications.style.fontWeight = 'bold';
+    publications.style.fontWeight = '800';
 });
 
 research.addEventListener('click', function(){
     navLinks.forEach(element => {
-        if (element.style.fontWeight = 'bold') {
+        if (element.style.fontWeight = '800') {
             element.style.fontWeight = '500';
         }
     });
-    research.style.fontWeight = 'bold';
+    research.style.fontWeight = '800';
 });
 
 data.addEventListener('click', function(){
     navLinks.forEach(element => {
-        if (element.style.fontWeight = 'bold') {
+        if (element.style.fontWeight = '800') {
             element.style.fontWeight = '500';
         }
     });
-    data.style.fontWeight = 'bold';
+    data.style.fontWeight = '800';
 });
 
 teaching.addEventListener('click', function(){
     navLinks.forEach(element => {
-        if (element.style.fontWeight = 'bold') {
+        if (element.style.fontWeight = '800') {
             element.style.fontWeight = '500';
         }
     });
-    teaching.style.fontWeight = 'bold';
+    teaching.style.fontWeight = '800';
 });
